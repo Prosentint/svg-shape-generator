890dc9cf20c6b8d6719f89f25191fd79c23d1644
Blackberry <david2631@gmail.com>
2023-10-02 13:22:42 -0400

    added return to each shape class and created inquirer prompt in index

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes');
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters for the text:',
+      validate: (text) =>
+        text.length <= 3 || "The message cannot be longer than 3 characters",
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter text color (keyword or hexadecimal number):',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter shape color (keyword or hexadecimal number):',
+    },
+  ]);
+};
+
+const generateSVG = (data) => {};
