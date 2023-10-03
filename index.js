3dcc092806868517f90577420bddb5841493d2a0
Blackberry <david2631@gmail.com>
2023-10-03 11:19:27 -0400

    added generative functionality, and adjusted the points of generated shapes, should generate an svg upon being called now

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes');
 
+// inquirer prompt to ask user questions
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -30,4 +31,54 @@ const promptUser = () => {
   ]);
 };
 
-const generateSVG = (data) => {};
+// will take user input and create an instance of shape based on those input and write to a file
+const generateSVG = (data) => {
+  // Create an instance of the selected shape class
+  let shape;
+  switch (data.shape) {
+    case 'circle':
+      shape = new Circle();
+      break;
+    case 'triangle':
+      shape = new Triangle();
+      break;
+    case 'square':
+      shape = new Square();
+      break;
+    default:
+      console.error('Error: Invalid shape. Please choose a valid shape.');
+      return;
+  }
+
+  // Set colors for the shape and text
+  shape.setColor(data.shapeColor);
+
+  // Create the SVG content based on the selected shape and user input
+  const svgContent = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${shape.render()}
+      <text x="100" y="120" font-size="30" fill="${data.textColor}">
+        ${data.text}
+      </text>
+    </svg>
+  `;
+
+  // Write the SVG content to a file named 'logo.svg'
+  fs.writeFileSync('logo.svg', svgContent);
+};
+
+// the start of the program
+const run = () => {
+  console.log('Welcome to the SVG Logo Maker!');
+  promptUser()
+    .then((data) => {
+      generateSVG(data);
+      console.log('Generated logo.svg');
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+};
+
+// calls the program to start
+run();
